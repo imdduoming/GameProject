6e89d0a67bd50d00bbec9f3302c2081cf4fbc20a
jh-lee-kor <govl9779@naver.com>
2020-05-29 02:15:38 +0900

    이장호 코드파일 업로드 200528 - 객체를 통한 개발 시작(일부수정)

--- gui.py
+++ gui.py
@@ -26,7 +26,7 @@ class StartPage(Frame):  # 시작 페이지, 버튼 위치 등은 사후 조정
         Button(self, text="게임종료", command=lambda: quit()).pack(anchor='center')
 
 
-class NewGame(Frame):
+class NewGame(Frame):  # 게임 페이지, 프레임을 띄웠다 내렸다 하는 형태로 레이아웃 구성할 예정, forget 이나 des tro y를 통해
     def __init__(self, master):
         Frame.__init__(self, master)
         Label(self, text='전체 게임 완성시 구성 예정').pack()
